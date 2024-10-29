a2be3b92df7d5f0ce3d990260eb742c3852caf8f
Bero <berislav.grgicak@gmail.com>
2024-10-29 15:16:45 +0100

    Rebuild 1

--- packages/playground/wordpress-builds/src/wordpress/get-wordpress-module-details.ts
+++ packages/playground/wordpress-builds/src/wordpress/get-wordpress-module-details.ts
@@ -38,7 +38,7 @@ export function getWordPressModuleDetails(wpVersion: string = "6.6"): { size: nu
 		case '6.6':
 			/** @ts-ignore */
 			return {
-				size: 18382656,
+				size: 18382231,
 				url: url_6_6,
 			};
 			
