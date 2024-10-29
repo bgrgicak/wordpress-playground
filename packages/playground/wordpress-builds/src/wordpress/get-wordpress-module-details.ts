828dc9f00e9d424ac87b6e31c0232bcef8351af3
Bero <berislav.grgicak@gmail.com>
2024-10-29 16:24:08 +0100

    Rebuild with -X 2 Still different sizes

--- packages/playground/wordpress-builds/src/wordpress/get-wordpress-module-details.ts
+++ packages/playground/wordpress-builds/src/wordpress/get-wordpress-module-details.ts
@@ -38,7 +38,7 @@ export function getWordPressModuleDetails(wpVersion: string = "6.6"): { size: nu
 		case '6.6':
 			/** @ts-ignore */
 			return {
-				size: 18281868,
+				size: 18281869,
 				url: url_6_6,
 			};
 			
