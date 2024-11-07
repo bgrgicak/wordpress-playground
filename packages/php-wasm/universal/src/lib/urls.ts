bd5d3a9d6c40e4466194027b0d379db0c9a0fdcf
Bero <berislav.grgicak@gmail.com>
2024-11-07 09:32:37 +0100

    Revert Use URL.canParse()

--- packages/php-wasm/universal/src/lib/urls.ts
+++ packages/php-wasm/universal/src/lib/urls.ts
@@ -69,5 +69,5 @@ export function ensurePathPrefix(path: string, prefix: string): string {
  * @returns True if the URL is absolute, false otherwise.
  */
 export function isAbsoluteUrl(url: string): boolean {
-	return URL.canParse(url);
+	return url.startsWith('http://') || url.startsWith('https://');
 }
